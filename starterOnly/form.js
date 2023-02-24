2c7cbf9bb25d0c6bd42797e994ae0d150fc61476
omar <omar.tourabi7@gmail.com>
2023-02-24 12:14:57 +0100

    close modal /Form a vérifer /css a terminer

---
+++ starterOnly/form.js
@@ -0,0 +1,122 @@
+class HandleForm {
+
+    fields = {
+      first: null,
+      last: null,
+      email: null,
+      birthdate: null,
+      quantity: null,
+      location: null,
+      checkbox1: document.getElementById('checkbox1').checked,
+      checkbox2: document.getElementById('checkbox2').checked,
+    }
+
+    errorMessage = {
+      first: "Veuillez entrer 2 caractères ou plus pour le champ du prenom.",
+      last: "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+      email: "Veuillez entrer une adresse email valide.",
+      birthdate: "Veuillez ajouter une date de naissance.",
+      quantity: "Veuillez indiquer le nombre de participation au tournoi.",
+      checkbox1: "Vous devez vérifier que vous acceptez les termes et conditions.",
+      checkbox2: "",
+      location: "Veuillez indiquer la ville de votre choix."
+    }
+
+    regEx = {
+      first: /^[a-zA-Z]{2,}$/,
+      last: /^[a-zA-Z]{2,}$/,
+      email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      birthdate: /^\d{4}-\d{2}-\d{2}$/,
+      quantity: /^\d+$/,
+      location: null,
+      checkbox1: null,
+      checkbox2: null,
+    }
+  
+    constructor(form) {
+      this.form = form
+      for (let key in this.fields){
+        switch(key){
+          case 'checkbox1': 
+          case 'checkbox2': 
+            new FormField(document.getElementById(key), this.regEx[key], this.handleCheckbox.bind(this))
+            break
+          case 'location':
+            new FormField(document.querySelector(`input[name="${key}"]`).parentNode, this.regEx[key], this.handleRadio.bind(this))
+            break
+          default:
+            new FormField(document.querySelector(`input[name="${key}"]`).parentNode, this.regEx[key], this.checkRegex.bind(this))
+            break
+        }
+      }
+      form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let canSubmit = true
+        for (let key in this.fields){
+          if (this.fields[key] === null || (key === 'checkbox1' && !this.fields[key])){
+            canSubmit = false
+            let target = `input[id="${key}"]`
+            if (key === 'location'){ //L'id de "location" a un chiffre après alors que le "name" non et inverse pour le checkbox
+              target = `input[name="${key}"]` 
+            }
+            const element = document.querySelector(target).parentNode
+            this.addError(key, element)
+          }
+        }
+        if (canSubmit){
+          e.target.style.display = 'none'
+          const modalValid = document.querySelector('.modal-valid')
+          modalValid.style.display = "flex"
+        }
+      })
+    }
+
+
+  handleRadio(e){
+    this.fields.location = e.target.value
+    this.removeAttribute(e.target.parentNode)
+  }
+    
+  handleCheckbox(e){
+    const {checked, id, parentNode: parent} = e.target
+    if (id === 'checkbox1'){
+      if (!checked){
+        this.addError(id, parent)
+      }else {
+        this.removeAttribute(parent)
+      }
+    }
+    this.fields[id] = checked
+  }
+
+  checkRegex(e, reg) {
+    const { value, id, parentNode: parent} = e.target
+    this.removeAttribute(parent)
+    if (reg.test(value)) {
+      this.fields[id] = value
+    } else {
+      this.addError(id, e.target.parentNode)
+    }
+  }
+  
+  removeAttribute(element){
+      element.removeAttribute('data-error-visible')
+      element.removeAttribute('data-error')
+  }
+  
+  addError(id, element){
+    this.fields[id] = null
+    element.setAttribute('data-error-visible', true)
+    element.setAttribute('data-error', this.errorMessage[id])
+  }
+
+}
+
+class FormField{
+  constructor(element, regex, handleField){
+    element.addEventListener('change', (e) => handleField(e, regex))
+  }
+}
+
+const form = document.querySelector('form');
+new HandleForm(form)
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,35 @@ function editNav() {
   }
 }
 
+// Variables
+const ANIMATION_DURATION = 300
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll('.closeModal')
+const closeAnimation = document.querySelector('.content')
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.forEach((btn) => btn.addEventListener('click', closeModal))
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form
+function closeModal() {
+  animationHandler(closeAnimation, "closeModal", ANIMATION_DURATION)
+  setTimeout(() => {
+    modalbg.style.display = "none";
+    animationHandler(closeAnimation)
+  }, ANIMATION_DURATION)
+}
+// Animation close modal
+function animationHandler(element, animationName = '', duration = '') {
+  element.style.animationName = animationName;
+  element.style.animationDuration = duration ? `${duration}ms` : '';
+}
